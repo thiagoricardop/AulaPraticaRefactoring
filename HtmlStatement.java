08ca954c65ae4af9d79ca77edbf2b81af558bf66
Thiago Ricardo <110063560+thiagoricardop@users.noreply.github.com>
2022-12-05 02:04:33 +0000

    Commit 17

--- HtmlStatement.java
+++ HtmlStatement.java
@@ -2,20 +2,26 @@ import java.util.Enumeration;
 
 public class HtmlStatement extends Statement {
 
-  public String value(Customer aCustomer) {
-    Enumeration rentals = aCustomer.getRentals();
-    String result = "<H1>Rentals for <EM>" + aCustomer.getName() + "</EM></H1><P>\n";
-    while (rentals.hasMoreElements()) {
-        Rental each = (Rental) rentals.nextElement();
-        // show figures for each rental
-        result += each.getMovie().getTitle()+ ": " +
-                String.valueOf(each.getCharge()) + "<BR>\n";
+    public String value(Customer aCustomer) {
+        Enumeration rentals = aCustomer.getRentals();
+        String result = getPrologue(aCustomer);
+        while (rentals.hasMoreElements()) {
+            Rental each = (Rental) rentals.nextElement();
+            result += getFigures(each);
+        }
+        result += getEpilogue(aCustomer);
+        return result;
     }
-    // add footer lines
-    result +=  "<P>You owe <EM>" + String.valueOf(aCustomer.getTotalCharge()) + "</EM><P>\n";
-    result += "On this rental you earned <EM>" +
-          String.valueOf(aCustomer.getTotalFrequentRenterPoints()) +
-          "</EM> frequent renter points<P>";
-    return result;
-  }
+    private String getPrologue(Customer aCustomer) {
+        return "<H1>Rentals for <EM>" + aCustomer.getName() + "</EM></H1><P>\n";
+    }
+    private String getFigures(Rental aRental) {
+        return aRental.getMovie().getTitle()+ ": " + String.valueOf(aRental.getCharge()) + "<BR>\n";
+    }
+    private String getEpilogue(Customer aCustomer) {
+        return "<P>You owe <EM>" + String.valueOf(aCustomer.getTotalCharge()) + "</EM><P>\n" +
+              "On this rental you earned <EM>" +
+              String.valueOf(aCustomer.getTotalFrequentRenterPoints()) +
+              "</EM> frequent renter points<P>";
+    }
 }
