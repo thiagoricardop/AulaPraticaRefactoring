1d9b072698d81c7dd9b8930e5ab597bb25ae491f
Thiago Ricardo <110063560+thiagoricardop@users.noreply.github.com>
2022-12-05 01:12:04 +0000

    Commit 6

--- Rental.java
+++ Rental.java
@@ -33,6 +33,11 @@ public class Rental {
           break;
       }
       return thisAmount;
-    }
+      }
+      public int getFrequentRenterPoints() {
+        if ((this.getMovie().getPriceCode() == Movie.NEW_RELEASE) && this.getDaysRented() > 1) 
+          return 2;
+        return 1;
+      }
  
 }
